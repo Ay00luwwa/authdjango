91fc626e3422fc0a0b067de34ea5a2201f27b9e1
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-05-10 15:02:56 +0100

    final edit

--- authApp/views.py
+++ authApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
-from .forms import CustomSignupForm, LoginForm
+from .forms import SignupForm, LoginForm
 
 
 # Create your views here.
@@ -9,28 +9,14 @@ def index(request):
     return render(request, 'index.html')
 
 # signup page
-# def user_signup(request):
-#     if request.method == 'POST':
-#         form = SignupForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('login')
-#     else:
-#         form = SignupForm()
-#     return render(request, 'signup.html', {'form': form})
-
 def user_signup(request):
     if request.method == 'POST':
-        form = CustomSignupForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')  # Replace 'home' with the name of your home URL pattern
+            return redirect('login')
     else:
-        form = CustomSignupForm()
+        form = SignupForm()
     return render(request, 'signup.html', {'form': form})
 
 # login page

4ca7512b062cd55fe0ff6b791f765d89c28b38da
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-05-10 14:41:04 +0100

    views edit

--- authApp/forms.py
+++ authApp/forms.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 class CustomSignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = ("password1", "password2")
+        fields = ("username","password", "password")
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-  
+    
--- authApp/views.py
+++ authApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
-from .forms import SignupForm, LoginForm
+from .forms import CustomSignupForm, LoginForm
 
 
 # Create your views here.
@@ -19,14 +19,14 @@ def index(request):
 #         form = SignupForm()
 #     return render(request, 'signup.html', {'form': form})
 
-def signup(request):
+def user_signup(request):
     if request.method == 'POST':
         form = CustomSignupForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')  # Replace 'home' with the name of your home URL pattern
     else:
@@ -34,4 +34,21 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 # login page
-def us
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
